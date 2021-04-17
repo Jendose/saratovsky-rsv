21fbe9697780e02b9660178e870190ffce0e029d
Vladimir Ermukhanov <ermuhanoff@gmail.com>
2021-04-17 13:58:59 +0400

    feat: commit

---
+++ saratovsky_rsv_ui/src/components/app/App.tsx
@@ -0,0 +1,15 @@
+import Container from "../container/Container";
+import Tmp from "../tmp/Tmp";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Container />
+
+      <Tmp />
+    </div>
+  );
+}
+
+export default App;
---
+++ saratovsky_rsv_ui/src/components/tmp/Tmp.tsx
@@ -0,0 +1,63 @@
+import "./Tmp.css"
+
+function Tmp() {
+  return (
+    <div className="wide-main">
+      <div className="rotor">
+        <div className="rim">
+          <div
+            className="item item1"
+          >
+            <div className="content router-link-active">
+              <div className="label">Главная</div>
+              <div className="image RMain-img"></div>
+            </div>
+          </div>
+          <div
+            className="item item2"
+          >
+            <div className="content">
+              <div className="label">Проекты</div>
+              <div className="image opportunities-img"></div>
+            </div>
+          </div>
+          <div
+            className="item item3"
+          >
+            <div className="content">
+              <div className="label">Обучение</div>
+              <div className="image pumps-img"></div>
+            </div>
+          </div>
+          <div
+            className="item item4"
+          >
+            <div className="content">
+              <div className="label">Трек развития</div>
+              <div className="image growth-img"></div>
+            </div>
+          </div>
+          <div
+            className="item item5"
+          >
+            <div className="content">
+              <div className="label">Истории успеха</div>
+              <div className="image examples-img"></div>
+            </div>
+          </div>
+          <div
+            className="item item6"
+          >
+            <div className="content">
+              <div className="label">Помощь другим</div>
+              <div className="image gains-img"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="field"></div>
+    </div>
+  );
+}
+
+export default Tmp;
